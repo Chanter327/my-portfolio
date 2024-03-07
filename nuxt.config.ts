110122b0260b954e183c635fabc7bc07b2e23755
Chanter327 <march09yuuto@gmail.com>
2024-03-07 10:29:01 +0900

    change config for github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: '/dist/'
+  },
   ssr: false,
   css: [
-    "~/assets/css/default.css"
+    "@/assets/css/default.css"
   ],
-  app: {
-    baseURL: '/kohiruimaki.github.io',
-  }
 })
