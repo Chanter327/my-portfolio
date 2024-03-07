ba8bad88509e13ef3db07f661f63ded0bcd2ea63
Chanter327 <march09yuuto@gmail.com>
2024-03-07 11:02:47 +0900

    change config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  app: {
-    baseURL: '/dist/'
-  },
   ssr: false,
   css: [
     "@/assets/css/default.css"
